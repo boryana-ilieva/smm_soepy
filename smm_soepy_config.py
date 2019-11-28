2d65e98b18bfbeea4a5e53570db4c695d594f76e
boryana-ilieva <boryana.a.ilieva@gmail.com>
2019-11-28 10:37:29 +0100

    Clean up.

---
+++ smm_soepy_config.py
@@ -0,0 +1,13 @@
+"""This module provides some configuration for the package."""
+import sys
+from pathlib import Path
+
+import numpy as np
+
+# We only support modern Python.
+np.testing.assert_equal(sys.version_info[:2] >= (3, 6), True)
+
+# We rely on relative paths throughout the package.
+PACKAGE_DIR = Path(__file__).parent.absolute()
+INIT_FILES_DIR = PACKAGE_DIR / "smm_estimagic" / "init_files"
+print(INIT_FILES_DIR)
